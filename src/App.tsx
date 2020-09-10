75889ba6f8e5e8ad52f864c3a76140b998825341
AdBoc <adrianboczon98@gmail.com>
2020-09-10 21:35:52 +0200

    Added D3 component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './chart-style.css';
 import ReactWay from "./Components/ReactWay";
+import D3Way from "./Components/D3Way";
 
 function App() {
   return (
-    <div>
+    <div className="charts">
       <ReactWay/>
+      <D3Way/>
     </div>
   );
 }
